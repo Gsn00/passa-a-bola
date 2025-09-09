9606cac93f432dd9367aeff78e696265e2649e83
Gabriel Novais <silvanovaesgabriel@gmail.com>
2025-09-09 19:05:58 -0300

    Ajustando tamanho do sectiontitle

--- src/components/sectiontitle/index.jsx
+++ src/components/sectiontitle/index.jsx
@@ -1,10 +1,10 @@
 export default function SectionTitle({ textgray, textpurple }) {
   return (
     <div className="max-lg:px-5 size-fit mx-auto mb-10">
-      <h1 className="text-[#696969]! text-5xl font-bold font-[League_Spartan]">
+      <h1 className="text-[#696969]! text-4xl lg:text-5xl font-bold font-[League_Spartan]">
         {textgray}
       </h1>
-      <h1 className="text-[#690A6C]! text-5xl font-bold font-[League_Spartan]">
+      <h1 className="text-[#690A6C]! text-4xl lg:text-5xl font-bold font-[League_Spartan]">
         {textpurple}
       </h1>
     </div>
