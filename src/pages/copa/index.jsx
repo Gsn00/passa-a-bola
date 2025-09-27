bcdcebb1e2aac5bb9a4536af8d307aa9916b00d8
Gabriel Novais <silvanovaesgabriel@gmail.com>
2025-09-27 01:16:32 -0300

    Hashtags na página copa

--- src/pages/copa/index.jsx
+++ src/pages/copa/index.jsx
@@ -48,7 +48,7 @@ export default function Copa() {
 
         <SectionTitle textgray="A Copa do" textpurple="Passa a Bola" />
 
-        <div className="flex flex-col lg:flex-row px-10 lg:px-30 mb-5 gap-10">
+        <div className="flex flex-col lg:flex-row px-5 lg:px-30 mb-5 gap-10">
           <motion.div
             ref={leftRef}
             className="relative w-full lg:w-[50%] border-5 border-[#f7edf7] bg-[#f7edf7]/55 rounded-4xl"
@@ -70,7 +70,7 @@ export default function Copa() {
             ref={rightRef}
             className="relative w-full lg:w-[50%] border-5 border-[#fbf7fb] bg-[#fbf7fb]/55 rounded-4xl sm:top-[85px]"
             initial={{ opacity: 0, y: 20 }}
-            animate={videoRightInView ? { opacity: 1, y: 0 } : {}}
+            animate={rightInView ? { opacity: 1, y: 0 } : {}}
             transition={{ duration: 0.5, delay: 0.8 }}
           >
             <p className="text-justify sm:text-xl w-full tracking-wide font-[League_Spartan] bg-[#f7edf7] p-8 rounded-4xl shadow-lg relative top-4 right-3">
@@ -83,43 +83,107 @@ export default function Copa() {
           </motion.div>
         </div>
 
-        <div className="flex flex-col lg:flex-row px-10 lg:px-30 md:mb-60 max-md:mt-10 gap-10">
-          <motion.div
-            ref={videoRightRef}
-            className="relative w-full border-5 border-[#f7edf7] bg-[#f7edf7]/55 rounded-4xl sm:top-[85px]"
-            initial={{ opacity: 0, y: 20 }}
-            animate={videoRightInView ? { opacity: 1, y: 0 } : {}}
-            transition={{ duration: 0.5, delay: 1.2 }}
-          >
-            <video
-              autoPlay
-              muted
-              loop
-              className="w-full h-[500px] md:h-[700px] rounded-4xl shadow-xl object-cover relative top-4 left-3"
+        <div className="flex flex-col lg:flex-row px-5 lg:px-30 max-md:mt-10 gap-10 md:pt-30">
+          <div className="w-full gap-5 text-center flex flex-col-reverse md:flex-col">
+            <motion.div
+              ref={videoLeftRef}
+              className="relative w-full border-5 border-[#f7edf7] bg-[#f7edf7]/55 rounded-4xl"
+              initial={{ opacity: 0, y: 20 }}
+              animate={videoLeftInView ? { opacity: 1, y: 0 } : {}}
+              transition={{ duration: 0.5, delay: 1.2 }}
             >
-              <source src="/videos/copa01.mp4" type="video/mp4" />
-            </video>
-          </motion.div>
+              <video
+                autoPlay
+                muted
+                loop
+                className="w-full h-[500px] md:h-[700px] rounded-4xl shadow-xl object-cover relative top-4 left-3"
+              >
+                <source src="/videos/copa01.mp4" type="video/mp4" />
+              </video>
+            </motion.div>
+            <div className="text-xl md:text-4xl">
+              <motion.h1
+                initial={{ opacity: 0, y: 20 }}
+                animate={videoLeftInView ? { opacity: 1, y: 0 } : {}}
+                transition={{ duration: 0.6, delay: 1.4 }}
+                className="font-[Montserrat] md:mr-10"
+              >
+                #MulheresNoEsporte
+              </motion.h1>
 
-          <motion.div
-            ref={videoLeftRef}
-            className="relative w-full border-5 border-[#fbf7fb] bg-[#fbf7fb]/55 rounded-4xl sm:top-[185px]"
-            initial={{ opacity: 0, y: 20 }}
-            animate={videoLeftInView ? { opacity: 1, y: 0 } : {}}
-            transition={{ duration: 0.5, delay: 1.4 }}
-          >
-            <video
-              autoPlay
-              muted
-              loop
-              className="w-full h-[500px] md:h-[700px] rounded-4xl shadow-xl object-cover relative top-4 right-3"
+              <motion.h1
+                initial={{ opacity: 0, scale: 0.8 }}
+                animate={videoLeftInView ? { opacity: 1, scale: 1 } : {}}
+                transition={{ duration: 0.6, delay: 1.6 }}
+                className="font-[Montserrat] text-[#690A6C]!"
+              >
+                #MulheresNoEsporte
+              </motion.h1>
+
+              <motion.h1
+                initial={{ opacity: 0 }}
+                animate={
+                  videoLeftInView ? { opacity: [0, 1, 0] } : { opacity: 0 }
+                }
+                transition={{ duration: 3, repeat: Infinity, delay: 2 }}
+                className="font-[Montserrat] md:ml-10 text-transparent! bg-clip-text bg-gradient-to-b from-[#cb6aaa]! to-transparent"
+              >
+                #MulheresNoEsporte
+              </motion.h1>
+            </div>
+          </div>
+
+          <div className="w-full space-y-5 text-center md:pt-15">
+            <div className="text-xl md:text-4xl">
+              <motion.h1
+                initial={{ opacity: 0, y: 20 }}
+                animate={videoLeftInView ? { opacity: 1, y: 0 } : {}}
+                transition={{ duration: 0.6, delay: 1.4 }}
+                className="font-[Montserrat] md:mr-10"
+              >
+                #FutebolFeminino
+              </motion.h1>
+
+              <motion.h1
+                initial={{ opacity: 0, scale: 0.8 }}
+                animate={videoLeftInView ? { opacity: 1, scale: 1 } : {}}
+                transition={{ duration: 0.6, delay: 1.6 }}
+                className="font-[Montserrat] text-[#690A6C]!"
+              >
+                #FutebolFeminino
+              </motion.h1>
+
+              <motion.h1
+                initial={{ opacity: 0 }}
+                animate={
+                  videoLeftInView ? { opacity: [0, 1, 0] } : { opacity: 0 }
+                }
+                transition={{ duration: 3, repeat: Infinity, delay: 2 }}
+                className="font-[Montserrat] md:ml-10 text-transparent! bg-clip-text bg-gradient-to-b from-[#cb6aaa]! to-transparent"
+              >
+                #FutebolFeminino
+              </motion.h1>
+            </div>
+            <motion.div
+              ref={videoRightRef}
+              className="relative w-full border-5 border-[#fbf7fb] bg-[#fbf7fb]/55 rounded-4xl"
+              initial={{ opacity: 0, y: 20 }}
+              animate={videoRightInView ? { opacity: 1, y: 0 } : {}}
+              transition={{ duration: 0.5, delay: 1.4 }}
             >
-              <source src="/videos/copa02.mp4" type="video/mp4" />
-            </video>
-          </motion.div>
+              <video
+                autoPlay
+                muted
+                loop
+                className="w-full h-[500px] md:h-[700px] rounded-4xl shadow-xl object-cover relative top-4 right-3"
+              >
+                <source src="/videos/copa02.mp4" type="video/mp4" />
+              </video>
+            </motion.div>
+          </div>
         </div>
 
-        <div className="text-center mt-15 sm:mt-40">
+        <div className="text-center mt-15">
           <ActionButton href="/copa/inscricao" title="Inscreva-se" />
         </div>
 
