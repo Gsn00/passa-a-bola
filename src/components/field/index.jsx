5612278e764e42aa279c4eea66d22193239796f0
Gabriel Novais <silvanovaesgabriel@gmail.com>
2025-09-28 16:27:56 -0300

    Alterando estilo do field

--- src/components/field/index.jsx
+++ src/components/field/index.jsx
@@ -1,6 +1,6 @@
 export default function Field(props) {
   return (
-    <div className="flex flex-col gap-1">
+    <div className="flex flex-col gap-1 w-full">
       <p className="font-bold text-sm">{props.title}</p>
       {props.children}
     </div>
